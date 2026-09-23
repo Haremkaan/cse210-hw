cdaf8aedb97fb43042d12c94f25cf1042f8092da
Haremkaan Michael Torkuma <hamtok564@gmail.com>
2026-09-23 02:32:26 -1100

    commit

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] splitText = text.Split(" ");
+        foreach (string wordText in splitText)
+        {
+            _words.Add(new Word(wordText));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random random = new Random();
+        for (int i = 0; i < count; i++)
+        {
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    } 
+
+    public string GetDisplayText()
+    {
+        string scriptureText = "";
+        foreach (Word word in _words)
+        {
+            scriptureText += word.GetDisplayText() + " ";
+        }
+        return $"{_reference.GetDisplayText()} - {scriptureText.Trim()}";
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
